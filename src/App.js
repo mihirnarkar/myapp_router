ac2f912f358700ad89dd43d140b52d9db71f9fb1
Mihir Narkar <Mihirnarkar19@gmail.com>
2022-09-11 12:54:41 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import React from 'react';
+import{
+  HashRouter,
+  Routes,
+  Route,
+} from "react-router-dom"
+import About from './components/About';
+import Home from './components/Home'
+// import ContactUs from './components/ContactUs';
+import Contact from './components/Contact';
 
 
 function App() {
   return (
-    <>
-    hello
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='about' element={<About/>} />
+        <Route path='contact' element={<Contact/>} />
+      </Routes>
+    </HashRouter>
   )
 }
 export default App;
